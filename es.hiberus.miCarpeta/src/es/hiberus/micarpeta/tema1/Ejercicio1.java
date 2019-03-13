59e56dd00d908af99217e07cbcb6e5765e1de8e6
Lupercar <hotolete@gmail.com>
2019-03-13 23:15:03 +0100

    Escribir por consola el String "Hola mundo".

---
+++ es.hiberus.miCarpeta/src/es/hiberus/micarpeta/tema1/Ejercicio1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.hiberus.micarpeta.tema1;
+
+/**
+ *
+ * @author MECAGOENTODO
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String holaMundo = "Hola mundo"; 
+        System.out.println(holaMundo);
+    }
+    
+}
