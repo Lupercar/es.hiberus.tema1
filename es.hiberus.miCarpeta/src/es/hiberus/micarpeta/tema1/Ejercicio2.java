f041b9f2a1a13f8480e1d65d02b83b31547e9ef6
Lupercar <hotolete@gmail.com>
2019-03-13 23:21:58 +0100

    Declare tres variables enteras a, b y c. Asignar el valor 4 y 5. Guardar la suma en c y mostrar la suma.

---
+++ es.hiberus.miCarpeta/src/es/hiberus/micarpeta/tema1/Ejercicio2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.hiberus.micarpeta.tema1;
+
+/**
+ * Declare tres variables enteras a, b y c. 
+ * Asignar el valor 4 y 5.
+ * Guardar la suma en c y mostrar la suma.
+ * @author MECAGOENTODO
+ */
+public class Ejercicio2 {
+    public static void main(String [] args){
+        int a,b,c;
+        a=4;
+        b=5;
+        c= a+b;
+        System.out.println(c);
+    }
+}
