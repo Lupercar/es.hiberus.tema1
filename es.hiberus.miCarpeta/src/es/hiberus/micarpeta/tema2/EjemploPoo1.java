022087db06f78ca8c60cf2e78ce1dd7162ea5437
Lupercar <hotolete@gmail.com>
2019-03-14 20:34:19 +0100

    Definicion de CLASE en POO

---
+++ es.hiberus.miCarpeta/src/es/hiberus/micarpeta/tema2/EjemploPoo1.java
@@ -0,0 +1,21 @@
+package es.hiberus.micarpeta.tema2;
+
+/**
+ * Clase es un conjunto de atributos y métodos
+ *
+ * @author MECAGOENTODO
+ */
+public class EjemploPoo1 {
+
+    int atributo1;
+    String atributo2;
+
+    void metodo1() {
+        //CODIGO DE LA ACCION1
+    }
+
+    void metodo2() {
+        //CODIGO DE LA ACCION2
+    }
+
+}
