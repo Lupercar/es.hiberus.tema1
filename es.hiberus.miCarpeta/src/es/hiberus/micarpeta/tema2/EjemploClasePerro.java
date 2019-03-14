395c24285a9cc8a838211cf0b5c7ca0b9f851847
Lupercar <hotolete@gmail.com>
2019-03-14 20:48:55 +0100

    Ejemplo de clase, clase PERRO

---
+++ es.hiberus.miCarpeta/src/es/hiberus/micarpeta/tema2/EjemploClasePerro.java
@@ -0,0 +1,23 @@
+package es.hiberus.micarpeta.tema2;
+
+/**
+ *  Clase que modela un PERRO
+ * @author MECAGOENTODO
+ */
+public class EjemploClasePerro {
+    
+    //atributos
+    String raza;
+    String color; 
+    double peso; 
+    int gradoObediencia; 
+    
+    //metodos o acciones
+    void alimentar(){
+        peso += 0.5; 
+    }
+    
+    void adiestrar(){
+        gradoObediencia++; 
+    }
+}
